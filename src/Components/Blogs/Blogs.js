81f5b9b818447095c13dfcc8c277d5c853a5d7e2
gazifahad <gazifahad3@gmail.com>
2022-04-05 20:38:26 +0600

    question answer added

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,7 +4,8 @@ const Blogs = () => {
     return (
         <div>
             <h1>Here are the blogs</h1>
-            <h2>context api?</h2>
+            <h2>what is semantic tag?</h2>
+            <p>Semantic tag is something that makes a web page meamningful to browser,It has a design that makes the browser to understand that what this tag will be used for, such as a header tag represents that the things inside it will be used as the header of a web page,A paragraph tag that is annoted with p clearly means that this tag will contain texts, on the other hand tags like b or i these doesnot mean anything to the browser, so these are not semantic tags.  </p>
         </div>
     );
 };
