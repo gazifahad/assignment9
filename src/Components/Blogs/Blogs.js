8982c7be5961dab9a6ca1c02ac068d7fc6e4631a
gazifahad <gazifahad3@gmail.com>
2022-04-05 21:05:18 +0600

    review section added and minor bugs fixed

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -10,6 +10,11 @@ const Blogs = () => {
                 1. Inline element only takes the height and width that it needs to contain,it doesnot start with a new line,it doesnot take any more spaces other than it's own.So we cannot set width and height of an inline element. <br></br>
                 2. block element takes the whole space in a line,It takes the whole width of it's parent , So everytime a block element starts in a new line,So we can set the height and width as per as we wish, <br></br>
                 3. Inline-Block elements are inline elements that behaves like block element,so it starts in the same line ,But we can set the height and width of it if it is necessary.
+            </p >
+            <h1 className='text-yellow-500 my-5 mx-5'>Context API?</h1>
+
+            <p className='text-left m-6'> We know in REACT the props can be delivered to childs from parents only.But for working purpose sometimes we need to pass from bottom to top also.The solution is context API.It is a way to make global variables that can be passed anywhere.SO we props can be passed from any level of the code to any other level without any hardship through context API.
+
             </p>
         </div>
         
